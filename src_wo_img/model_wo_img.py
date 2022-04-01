6cf3b5f698c53380f11bf9ad52289b6150b5ae79
Aman-4-Real <335955781@qq.com>
2022-04-01 15:12:56 +0800

    4.1 update: add src_wo_img codes

--- src_wo_img/model_wo_img.py
+++ src_wo_img/model_wo_img.py
@@ -1,16 +1,15 @@
 '''
 Author: Aman
-Date: 2022-01-13 00:50:07
+Date: 2022-01-14 22:19:05
 Contact: cq335955781@gmail.com
 LastEditors: Aman
-LastEditTime: 2022-01-21 23:41:13
+LastEditTime: 2022-04-01 15:00:15
 '''
 
-
-import pdb
 import torch
 import torch.nn as nn
 import torch.nn.init as init
+from scipy import stats
 import random
 import math
 import pickle
@@ -33,8 +32,8 @@ class MultiModalEncoder(nn.Module):
         self.text_input_dim = model_cfgs['text']['input_dim']
         self.text_hidden_dim = model_cfgs['text']['hidden_dim']
         self.text_num_layers = model_cfgs['text']['num_layers']
-        # assert self.topic_hidden_dim == self.image_hidden_dim == self.text_hidden_dim, \
-        #     "The hidden dim of topic, image and text must be equal."
+        assert self.topic_hidden_dim == self.text_hidden_dim, \
+            "The hidden dim of topic and text must be equal."
         # for topic mlp
         self.topic_fc = nn.Linear(self.topic_input_dim, self.topic_hidden_dim)
         # for image multi-layer rnns
@@ -58,7 +57,7 @@ class MultiModalEncoder(nn.Module):
             self.rnns_text = nn.GRU(self.text_input_dim, self.text_hidden_dim, \
                                     num_layers=self.text_num_layers, dropout=self.dropout_rate)
         
-        self.dropout = nn.Dropout(self.dropout_rate)
+        # self.dropout = nn.Dropout(self.dropout_rate)
         self.init_weights()
 
     def forward(self, encoder_batch):
@@ -90,12 +89,12 @@ class MultiModalEncoder(nn.Module):
         init.orthogonal_(self.rnns_text.weight_hh_l0)
 
 
-class SelfAttentionLayer(nn.Module):
+class InnerModalAttentionLayer(nn.Module):
     def __init__(self, model_cfgs):
         '''
         Compute the self attention of the hidden states of the image and text inputs.
         '''
-        super(SelfAttentionLayer, self).__init__()
+        super(InnerModalAttentionLayer, self).__init__()
         self.hidden_size = model_cfgs['SELF_ATT']['hidden_size']
         self.attention_heads = model_cfgs['SELF_ATT']['attention_heads']
         self.dropout_rate = model_cfgs['dropout']
@@ -112,7 +111,15 @@ class SelfAttentionLayer(nn.Module):
         self.value = nn.Linear(self.hidden_size, self.all_head_size)
         
         # dropout
-        self.dropout = nn.Dropout(self.dropout_rate)
+        # self.dropout = nn.Dropout(self.dropout_rate)
+
+        # define normal distribution
+        self.normal_dists = []
+        for i in range(5):
+            normal_values = stats.norm.pdf(np.arange(0,5,1), i, 1)
+            normal_values = torch.tensor([item/sum(normal_values) for item in normal_values], dtype=torch.float32)
+            self.normal_dists.append(normal_values)
+        self.kl_loss = nn.KLDivLoss(reduction='batchmean')
 
     def reshape_for_scores(self, x):
         '''
@@ -122,6 +129,7 @@ class SelfAttentionLayer(nn.Module):
         '''
         new_x_shape = x.size()[:-1] + (self.attention_heads, self.attention_head_size) # [bs, seq_len, attention_heads, attention_head_size]
         x = x.contiguous().view(*new_x_shape)
+        # print("x:", x)
         return x.permute(0, 2, 1, 3).contiguous() # [bs, attention_heads, seq_len, attention_head_size]
 
     def forward(self, input):
@@ -132,44 +140,52 @@ class SelfAttentionLayer(nn.Module):
         mixed_query_layer = self.query(input)
         mixed_key_layer = self.key(input)
         mixed_value_layer = self.value(input) # [bs, seq_len, hidden_size]
-
+        
         query_layer = self.reshape_for_scores(mixed_query_layer)
         key_layer = self.reshape_for_scores(mixed_key_layer)
         value_layer = self.reshape_for_scores(mixed_value_layer) # [bs, attention_heads, seq_len, attention_head_size]
-
+        # print("/////")
+        # print('value_layer:', value_layer)
+        
         attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2)) # [bs, attention_heads, seq_len, seq_len]
         attention_scores = attention_scores / math.sqrt(self.attention_head_size) # [bs, attention_heads, seq_len, seq_len]
-
+        # print(attention_scores)
         attention_probs = nn.Softmax(dim=-1)(attention_scores) # [bs, attention_heads, seq_len, seq_len]
-
-        attention_probs = self.dropout(attention_probs)
+        # print(attention_probs)
+        # attention_probs = self.dropout(attention_probs)
+        kldivloss = torch.zeros(input.size(1)).to(attention_probs.device)
+        for i in range(input.size(1)): # seq_len
+            kldivloss[i] = self.kl_loss(attention_probs[:,:,i,:].log(), \
+                self.normal_dists[i].to(attention_probs.device).unsqueeze(0).unsqueeze(0).repeat(attention_probs.size(0), attention_probs.size(1), 1))
         
+        # print('attention_probs:', attention_probs)
         # [bs, attention_heads, seq_len, seq_len] * [bs, attention_heads, seq_len, attention_head_size] = [bs, attention_heads, seq_len, attention_head_size]
         context_layer = torch.matmul(attention_probs, value_layer) # [bs, attention_heads, seq_len, attention_head_size]
+        
         context_layer = context_layer.permute(0, 2, 1, 3).contiguous() # [bs, seq_len, attention_heads, attention_head_size]
         new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,) # [bs, seq_len, out_hidden_size]
         context_layer = context_layer.contiguous().view(*new_context_layer_shape)
+        
+        return context_layer, kldivloss.mean() # [bs, seq_len, out_hidden_size]
 
-        return context_layer # [bs, seq_len, out_hidden_size]
 
 
 class MultiModalAttentionLayer(nn.Module):
     def __init__(self, model_cfgs):
         '''
-        Using topic input as query to compute the weighted sum of each time step of image and text modality.
+        Computing the weighted sum of each time step of image and text modality.
         '''
         super(MultiModalAttentionLayer, self).__init__()
         self.seq_len = model_cfgs['seq_len']
-        self.dropout_rate = model_cfgs['dropout']
         self.topic_hidden_dim = model_cfgs['topic']['hidden_dim']
         self.attention_dim = model_cfgs['MM_ATT']['attention_dim']
         self.att_input_dim = self.topic_hidden_dim
 
-        # topic as query
-        self.topic_q = nn.Linear(self.att_input_dim, self.attention_dim)
-        # self.k = nn.ModuleList([nn.Linear(self.att_input_dim, self.attention_dim) for i in range(self.seq_len)])
-        self.v = nn.ModuleList([nn.Linear(self.att_input_dim, self.attention_dim) for i in range(self.seq_len)])
-        self._norm_fact = 1 / math.sqrt(self.attention_dim)
+        self.att_matrices = nn.ModuleList([
+            nn.Linear(self.att_input_dim, self.attention_dim) for i in range(self.seq_len)
+            ])
+        # self._norm_fact = 1 / math.sqrt(self.attention_dim)
+        self.out_linear = nn.Linear(self.att_input_dim, 2048)
 
     def forward(self, topic_output, text_output):
         '''
@@ -177,56 +193,23 @@ class MultiModalAttentionLayer(nn.Module):
             topic_output: [1, batch_size, hidden_dim]
             text_output: [seq_len, batch_size, hidden_dim]
         '''
-        Q = self.topic_q(topic_output).transpose(0, 1) # Q: [batch_size, 1, attention_dim]
         batch_size = text_output.size(1)
         device = text_output.device
         # Attention
-        atten_outputs = torch.zeros(self.seq_len, batch_size, self.attention_dim).to(device)
+        atten_outputs = torch.zeros(self.seq_len, batch_size, 2048).to(device)
         for i in range(self.seq_len):
-            atten_input = torch.cat([topic_output, text_output[i,:,:].unsqueeze(0)], dim=0) # [2, batch_size, hidden_dim]
-            V = self.v[i](atten_input).transpose(0, 1) # [2, batch_size, hidden_dim] => [batch_size, 2, attention_dim]
-            atten = nn.Softmax(dim=-1)(torch.bmm(Q, V.permute(0,2,1)) * self._norm_fact) # [batch_size, 1, attention_dim] * [batch_size, attention_dim, 2] => [batch_size, 1, 2]
-            output = torch.bmm(atten, V) # [batch_size, 1, 2] * [batch_size, 2, attention_dim] => [batch_size, 1, attention_dim]
-            atten_outputs[i,:,:] = output.transpose(0, 1) # [batch_size, 1, attention_dim] => [1, batch_size, attention_dim]
+            topic_att = self.att_matrices[i](topic_output).transpose(0, 1) # [1, batch_size, hidden_dim] => [batch_size, 1, attention_dim]
+            # image_att = self.att_matrices[i](image_output[i,:,:].unsqueeze(0)).transpose(0, 1) # [1, batch_size, hidden_dim] => [batch_size, 1, attention_dim]
+            text_att = self.att_matrices[i](text_output[i,:,:].unsqueeze(0)).transpose(0, 1) # [1, batch_size, hidden_dim] => [batch_size, 1, attention_dim]
+            atten = nn.Softmax(dim=-1)(torch.cat([topic_att, text_att], dim=1).permute(0,2,1)) # [batch_size, attention_dim, 3] => [batch_size, 1, 3]
+            output = torch.bmm(atten, torch.cat([topic_output.transpose(0, 1), \
+                        text_output[i,:,:].unsqueeze(0).transpose(0, 1)], dim=1)) # [batch_size, 1, 3] * [batch_size, 3, hidden_dim] => [batch_size, 1, hidden_dim]
+            # print(atten)
+            atten_out = self.out_linear(output) # [batch_size, 1, hidden_dim] => [batch_size, 1, attention_dim]
+            atten_outputs[i,:,:] = atten_out.transpose(0, 1) # [batch_size, 1, attention_dim] => [1, batch_size, attention_dim]
         
         return atten_outputs # [seq_len, batch_size, attention_dim]
 
-
-class AggregateLayer(nn.Module):
-    def __init__(self, model_cfgs):
-        '''
-        Aggregate the attention outputs.
-        '''
-        super(AggregateLayer, self).__init__()
-        self.type = model_cfgs['agg']['type']
-        self.input_dim = model_cfgs['MM_ATT']['attention_dim']
-        self.output_dim = model_cfgs['agg']['output_dim']
-        self.agg_num_layers = model_cfgs['agg']['num_layers']
-        self.dropout_rate = model_cfgs['dropout']
-
-        # for aggregate multi-layer rnns
-        if model_cfgs['agg']['type'] == 'RNN':
-            self.agg_layer = nn.RNN(self.input_dim, self.output_dim, num_layers=self.agg_num_layers, nonlinearity = "relu", dropout=self.dropout_rate)
-        elif model_cfgs['agg']['type'] == 'LSTM':
-            self.agg_layer = nn.LSTM(self.input_dim, self.output_dim, num_layers=self.agg_num_layers, dropout=self.dropout_rate)
-        elif model_cfgs['agg']['type'] == 'GRU':
-            self.agg_layer = nn.GRU(self.input_dim, self.output_dim, num_layers=self.agg_num_layers, dropout=self.dropout_rate)
-        
-        self.init_weights()
-
-    def forward(self, input):
-        '''
-        Args:
-            input (self_attention_output): [seq_len, bs, attention_dim]
-        '''
-        self.agg_layer.flatten_parameters()
-        output_agg, hidden_agg = self.agg_layer(input)
-        
-        return output_agg # [seq_len, batch_size, agg_output_dim]
-
-    def init_weights(self):
-        init.xavier_normal_(self.agg_layer.weight_ih_l0)
-        init.orthogonal_(self.agg_layer.weight_hh_l0)
 
 
 class GPT2_Decoder(nn.Module):
@@ -265,7 +248,6 @@ class GPT2_Decoder(nn.Module):
             attention_mask: [batch_size, seq_len * _sent_length * 2]
             type_ids: [batch_size, seq_len * _sent_length * 2]
         '''
-        
         # process labels
         prompt_length = topic_ids.size(1) + concat_output.size(1)
         labels = torch.cat([torch.zeros(prompt_length, dtype=torch.long).unsqueeze(0).repeat(concat_output.size(0),1).to(input_ids.device), input_ids], dim=1)
@@ -311,8 +293,10 @@ class GPT2_Decoder(nn.Module):
                 return_dict=True
             ) # ['loss', 'logits', 'past_key_values']
         else:
+            # import pdb; pdb.set_trace()
             res = self.gpt2(
                 inputs_embeds=gpt_input_embs,
+                # token_type_ids=type_ids[:,:gpt_input_embs.size(1)],
                 labels=labels,
                 return_dict=True
             )
@@ -329,10 +313,10 @@ class EXPTeller(nn.Module):
         self.ln_layer1 = torch.nn.LayerNorm(model_cfgs['topic']['hidden_dim'], elementwise_affine=True)
         # self.ln_layer2 = torch.nn.LayerNorm(model_cfgs['image']['hidden_dim'], elementwise_affine=True)
         self.ln_layer3 = torch.nn.LayerNorm(model_cfgs['text']['hidden_dim'], elementwise_affine=True)
-        # self.img_self_atten_layer = SelfAttentionLayer(model_cfgs)
-        self.text_self_atten_layer = SelfAttentionLayer(model_cfgs)
+        # self.img_inner_atten_layer = InnerModalAttentionLayer(model_cfgs)
+        self.text_inner_atten_layer = InnerModalAttentionLayer(model_cfgs)
         self.mm_atten_layer = MultiModalAttentionLayer(model_cfgs)
-        self.agg_layer = AggregateLayer(model_cfgs)
+        # self.agg_layer = AggregateLayer(model_cfgs)
         self.decoder = GPT2_Decoder()
         self.train_flag = train_flag
         if train_flag:
@@ -362,49 +346,56 @@ class EXPTeller(nn.Module):
             }
         '''
         encoder_batch = {'topic': batch['topic_emb'].float(), \
-                         'image': batch['img_embs'].transpose(0, 1).float(), \
+                         'image': batch['r_embs'].transpose(0, 1).float(), \
                          'text': batch['r_embs'].transpose(0, 1).float()}
-        batch_size = batch['img_embs'].size(0)
-        device = batch['img_embs'].device
-        seq_len = batch['img_embs'].size(1)
+        batch_size = batch['r_embs'].size(0)
+        device = batch['r_embs'].device
+        seq_len = batch['r_embs'].size(1)
         # output_len = batch['targets'].size(2)
-
-        # Multi-modal Encoder
+        
+        # ===== Multi-modal Encoder =====
         topic_output, text_output = self.encoder(encoder_batch)
         # topic_output: [1, batch_size, hidden_dim], image_output, text_output: [seq_len, batch_size, hidden_dim]
+        # print("topic_output: ", topic_output)
         
         topic_output = self.ln_layer1(topic_output)
         # image_output = self.ln_layer2(image_output)
         text_output = self.ln_layer3(text_output)
-
-        # Self Attention Layer
-        # img_self_attention_output = self.img_self_atten_layer(image_output.transpose(0, 1))
-        text_self_attention_output = self.text_self_atten_layer(text_output.transpose(0, 1))
+        
+        # ===== Self Attention Layer =====
+        # print("img att: ")
+        # img_inner_attention_output, img_kl_loss = self.img_inner_atten_layer(image_output.transpose(0, 1))
+        # print("text att: ")
+        text_inner_attention_output, text_kl_loss = self.text_inner_atten_layer(text_output.transpose(0, 1))
         # [batch_size, seq_len, attention_dim] => [batch_size, seq_len, attention_dim]
+        
+        # print("img_kl_loss:", img_kl_loss, "text_kl_loss:", text_kl_loss)
+        # print(img_self_attention_output)
 
-        # Multi-modal Attention Layer
-        mm_attention_output = self.mm_atten_layer(topic_output, \
-                                text_self_attention_output.transpose(0,1))
+
+        # ===== Multi-modal Attention Layer =====
+        mm_attention_output = self.mm_atten_layer(topic_output, text_inner_attention_output.transpose(0,1))
         # [seq_len, batch_size, attention_dim]
-
-        # Aggregate
-        output_agg = self.agg_layer(mm_attention_output).transpose(0, 1)
+        
+        # ===== Aggregate =====
+        # output_agg = self.agg_layer(mm_attention_output).transpose(0, 1)
         # [seq_len, batch_size, attention_dim] => [batch_size, seq_len, attention_dim]
 
-        # Concatenate topic_output and output_agg
-        topic_output_repeat = topic_output.repeat(seq_len, 1, 1).transpose(0, 1) # [batch_size, seq_len, hidden_dim]
-        concat_output = torch.cat([topic_output_repeat, output_agg], dim=-1) # [batch_size, seq_len, hidden_dim + attention_dim]
+        # ===== Concatenate topic_output and output_agg =====
+        # topic_output_repeat = topic_output.repeat(seq_len, 1, 1).transpose(0, 1) # [batch_size, seq_len, hidden_dim]
+        # concat_output = torch.cat([topic_output_repeat, output_agg], dim=-1) # [batch_size, seq_len, hidden_dim + attention_dim]
+        # print(concat_output)
 
-        # Decoder
+        # ===== Decoder =====
         decoder_input = batch['targets'] # [batch_size, seq_len * _sent_length * 2]
         # outputs = torch.zeros(batch_size, seq_len, decoder_input.shape[2], self.vocab_size).to(device)
         # loss = torch.zeros(batch_size, seq_len).to(device)
 
-        res = self.decoder(concat_output, decoder_input, \
+        res = self.decoder(mm_attention_output.transpose(0, 1), decoder_input, \
                            batch['topic_ids'], batch['tpw_attention_mask'], batch['tpw_type_ids'], \
                            batch['attention_mask'], batch['type_ids'], self.train_flag)
         loss, outputs = res['loss'], res['logits']
 
-        return loss, outputs # [batch_size, seq_len+_max_seq_length, vocab_size]
+        return loss, text_kl_loss.mean(), outputs # [batch_size, seq_len+_max_seq_length, vocab_size]
         
 
